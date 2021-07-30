5f5265bd676cd29b849f1e33f123fe79ac3f4129
mah2021 <80781844+mah2021@users.noreply.github.com>
2021-07-30 16:14:38 +0200

    Add files via upload

---
+++ countdown-timer/script.js
@@ -0,0 +1,26 @@
+daysEl=document.getElementById('days');
+hoursEl=document.getElementById('hours');
+minsEl=document.getElementById('mins');
+secondsEl=document.getElementById('seconds');
+const newYear='1 Jan 2022';
+function countdown(){
+    const newYearDate=new Date(newYear);
+    const currentDate = new Date();
+    const totalseconds = (newYearDate-currentDate)/1000;
+    const days = Math.floor(totalseconds/3600/24);
+    const hours = Math.floor(totalseconds/3600) % 24;
+    const mins = Math.floor(totalseconds/60)  % 60;
+    const seconds = Math.floor(totalseconds) % 60;
+    
+    console.log(days , hours , mins ,seconds);
+    daysEl.innerHTML=days;
+    hoursEl.innerHTML=formatTime(hours);
+    minsEl.innerHTML=formatTime(mins);
+    secondsEl.innerHTML=formatTime(seconds);
+}
+function formatTime(time){
+    return time <10 ? `0${time}` : time ;
+}
+countdown();
+
+setInterval(countdown,1000);
